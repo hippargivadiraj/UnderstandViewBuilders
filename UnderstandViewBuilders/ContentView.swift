aceb233b691165eaba8fe24ea6b4236143de5fa2
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-06-03 20:04:04 -0700

    Initial Commit

---
+++ UnderstandViewBuilders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnderstandViewBuilders
+//
+//  Created by Vadiraj Hippargi on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnderstandViewBuilders/UnderstandViewBuildersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandViewBuildersApp.swift
+//  UnderstandViewBuilders
+//
+//  Created by Vadiraj Hippargi on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandViewBuildersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
